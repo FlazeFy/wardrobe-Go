bc5f5b029c7240693e4107052b9f5793e2cd03e4
Leonardho Rante S <flazen.edu@gmail.com>
2025-05-22 06:00:10 +0700

    [TEST] Integration Test for API Get All Clothes Header, Get Dictionary By Type

--- wardrobe/tests/integration_test/clothes_test.go
+++ wardrobe/tests/integration_test/clothes_test.go
@@ -30,6 +30,12 @@ type ResponseGetUsedHistory struct {
 	Status  string                      `json:"status"`
 }
 
+type ResponseGetClothesHeader struct {
+	Data    []models.ClothesHeader `json:"data"`
+	Message string                 `json:"message"`
+	Status  string                 `json:"status"`
+}
+
 func TestGetClothesLastHistory(t *testing.T) {
 	var res ResponseGetClothesLastHistory
 	url := "http://127.0.0.1:9000/api/v2/clothes/last_history"
@@ -119,8 +125,66 @@ func TestGetClothesUsedHistory(t *testing.T) {
 		assert.IsType(t, "", dt.UsedContext)
 		assert.IsType(t, time.Time{}, dt.CreatedAt)
 		if dt.ClothesNote != nil {
-			assert.NotEmpty(t, dt.ClothesNote)
+			assert.IsType(t, "", dt.ClothesNote)
+		}
+	}
+}
+
+func TestGetAllClothesHeader(t *testing.T) {
+	var res ResponseGetClothesHeader
+	url := "http://127.0.0.1:9000/api/v2/clothes/header/all/desc"
+	token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDgzODk1NjAsImlhdCI6MTc0Nzc4NDc2MCwidXNlcl9pZCI6IjU0YWYyOTEzLTVkMjEtNDgyOC05NDZhLTg2YTYyZTA1OGU1NCJ9.__Ag7Rcz-HInB4pjTzIN2KbBvGdulQIN8jmiU3Y9IZQ"
+
+	// Exec
+	req, err := http.NewRequest("GET", url, nil)
+	assert.NoError(t, err)
+	req.Header.Set("Authorization", "Bearer "+token)
+	resp, err := http.DefaultClient.Do(req)
+	assert.NoError(t, err)
+	defer resp.Body.Close()
+
+	// Prepare Test
+	body, err := ioutil.ReadAll(resp.Body)
+	assert.NoError(t, err)
+	err = json.Unmarshal(body, &res)
+	assert.NoError(t, err)
+
+	// Get Template Test
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	assert.NotEmpty(t, res.Status)
+	assert.Equal(t, "success", res.Status)
+	assert.NotEmpty(t, res.Message)
+	assert.Equal(t, "clothes fetched", res.Message)
+	assert.NotNil(t, res.Data)
+
+	for _, dt := range res.Data {
+		// Check Object
+		assert.NotEqual(t, uuid.Nil, dt.ID)
+		assert.NotEmpty(t, dt.ClothesName)
+		assert.NotEmpty(t, dt.ClothesColor)
+		assert.NotEmpty(t, dt.ClothesQty)
+		assert.NotEmpty(t, dt.ClothesType)
+		assert.NotEmpty(t, dt.ClothesCategory)
+		assert.NotEmpty(t, dt.ClothesSize)
+		assert.NotEmpty(t, dt.ClothesGender)
+		if dt.ClothesImage != nil {
+			assert.NotEmpty(t, dt.ClothesImage)
 		}
 
+		// Check Data Type
+		assert.IsType(t, "", dt.ClothesName)
+		assert.IsType(t, "", dt.ClothesColor)
+		assert.IsType(t, "", dt.ClothesType)
+		assert.IsType(t, "", dt.ClothesCategory)
+		assert.IsType(t, "", dt.ClothesSize)
+		assert.IsType(t, "", dt.ClothesGender)
+		assert.IsType(t, true, dt.IsFaded)
+		assert.IsType(t, true, dt.HasWashed)
+		assert.IsType(t, true, dt.HasIroned)
+		assert.IsType(t, true, dt.IsFavorite)
+		assert.IsType(t, true, dt.IsScheduled)
+		if dt.ClothesImage != nil {
+			assert.IsType(t, "", dt.ClothesImage)
+		}
 	}
 }
--- wardrobe/tests/integration_test/dictionary_test.go
+++ wardrobe/tests/integration_test/dictionary_test.go
@@ -2,6 +2,7 @@ package integrationtest
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"testing"
@@ -21,6 +22,48 @@ type ResponseGetAllDictionary struct {
 func TestGetAllDictionary(t *testing.T) {
 	var res ResponseGetAllDictionary
 	url := "http://127.0.0.1:9000/api/v2/dictionary"
+	token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDgzODk1NjAsImlhdCI6MTc0Nzc4NDc2MCwidXNlcl9pZCI6IjU0YWYyOTEzLTVkMjEtNDgyOC05NDZhLTg2YTYyZTA1OGU1NCJ9.__Ag7Rcz-HInB4pjTzIN2KbBvGdulQIN8jmiU3Y9IZQ"
+
+	// Exec
+	req, err := http.NewRequest("GET", url, nil)
+	assert.NoError(t, err)
+	req.Header.Set("Authorization", "Bearer "+token)
+	resp, err := http.DefaultClient.Do(req)
+	assert.NoError(t, err)
+	defer resp.Body.Close()
+
+	// Prepare Test
+	body, err := ioutil.ReadAll(resp.Body)
+	assert.NoError(t, err)
+	err = json.Unmarshal(body, &res)
+	assert.NoError(t, err)
+
+	// Get Template Test
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	assert.NotEmpty(t, res.Status)
+	assert.Equal(t, "success", res.Status)
+	assert.NotEmpty(t, res.Message)
+	assert.Equal(t, "dictionary fetched", res.Message)
+	assert.NotNil(t, res.Data)
+
+	for _, dt := range res.Data {
+		// Check Object
+		assert.NotEqual(t, uuid.Nil, dt.ID)
+		assert.NotEmpty(t, dt.DictionaryType)
+		assert.NotEmpty(t, dt.DictionaryName)
+		assert.NotEmpty(t, dt.CreatedAt)
+
+		// Check Data Type
+		assert.IsType(t, "", dt.DictionaryName)
+		assert.IsType(t, "", dt.DictionaryType)
+		assert.IsType(t, time.Time{}, dt.CreatedAt)
+	}
+}
+
+func TestGetDictionaryByType(t *testing.T) {
+	var res ResponseGetAllDictionary
+	dictionary_type := "clothes_type"
+	url := fmt.Sprintf("http://127.0.0.1:9000/api/v2/dictionary/%s", dictionary_type)
 	token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDgzODk1NjAsImlhdCI6MTc0Nzc4NDc2MCwidXNlcl9pZCI6IjU0YWYyOTEzLTVkMjEtNDgyOC05NDZhLTg2YTYyZTA1OGU1NCJ9.__Ag7Rcz-HInB4pjTzIN2KbBvGdulQIN8jmiU3Y9IZQ"
 
 	// Exec
